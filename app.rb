26bb75539bc9841768f83c607bc2108e22ec1879
nafan <nafanpzk@mail.ru>
2016-04-14 15:41:44 +0300

    add index view

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ end
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
 
 get '/new' do
@@ -40,6 +40,7 @@ post '/new' do
 		return erb :new
 	end
 
+	# сохранение данных в базу
 	@db.execute 'INSERT INTO posts (content, created_date) VALUES (?, datetime())', [content]
 
 	erb "You typed #{content}"
